b211f95995c6572067f658b82ebfd4c3bd8e7b1d
Muhammad Adnan Zafar <160720380+Madnanzafard@users.noreply.github.com>
2024-03-24 16:22:19 +0500

    Add files via upload

---
+++ Day 13/question37.js
@@ -0,0 +1,10 @@
+"use strict";
+/*Question 37: Large Shirts: Default values in make_shirt().
+Explain & TIP: Modify functions to have default parameters, simplifying calls for common use cases.
+This introduces default function parameters.*/
+function make_shirt(size = "large", message = "I love TypeScript") {
+    console.log(`The shirt size is ${size} and the message is ${message}`);
+}
+make_shirt();
+make_shirt("medium");
+make_shirt("small", "I love programming");
